7b66510671dba9dae3f3fbbfee5b80f3f2664b65
deDSeC00720 <terminator.karansharma@gmail.com>
2022-09-24 20:30:10 +0530

    add fraction class

---
+++ OOPS/fraction.h
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+
+class Fraction {
+    int numerator;
+    int denominator;
+
+    public:
+    
+    Fraction(int numerator, int denominator) {
+        this -> numerator = numerator;
+        this -> denominator = denominator;
+    }
+
+    void print() {
+        cout << this -> numerator << " / " << denominator << '\n';
+    }
+
+    void simplify() {
+        int gcd = 1;
+        int j = min(numerator, denominator);
+        for (int i = 1; i <= j; i++) {
+            if (numerator % i == 0 && denominator % i == 0) {
+                gcd = i;
+            }
+        }
+        numerator /= gcd;
+        denominator /= gcd;
+    }
+
+    void add(Fraction const &f2) {
+        int lcm = denominator * f2.denominator;
+        int x = lcm / denominator;
+        int y = lcm / f2.denominator;
+
+        int num = x * numerator + (y * f2.numerator);
+
+        numerator = num;
+        denominator = lcm;
+
+        simplify();
+    }
+
+    void multiply(Fraction const &f2) {
+        numerator *= f2.numerator;
+        denominator *= f2.denominator;
+
+        simplify();
+    }
+};
