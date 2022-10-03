85a47763b81de10f1f532944243375252e77568d
deDSeC00720 <terminator.karansharma@gmail.com>
2022-10-03 20:05:31 +0530

    take input level wise in tree

--- data-structures/trees/tree/tree.cpp
+++ data-structures/trees/tree/tree.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 int main() {
-    TreeNode<int> *root = takeInputRecursive();
+    TreeNode<int> *root = takeInputLevelWise();
 
     printTreeRecursive(root);
 }
--- data-structures/trees/tree/tree.h
+++ data-structures/trees/tree/tree.h
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "../../../common/treeNode.h"
+#include "../../queue/queue-using-LL/queue.h"
 
 TreeNode<int>* takeInputRecursive() {
     int data;
@@ -14,6 +15,33 @@ TreeNode<int>* takeInputRecursive() {
 
     for (int i = 0; i < n; i++) {
         root -> children.push_back(takeInputRecursive());
+    }
+    return root;
+}
+
+TreeNode<int>* takeInputLevelWise() {
+    int data;
+    std::cout << "Enter root: ";
+    std::cin >> data;
+    TreeNode<int> *root = new TreeNode<int>(data);
+
+    Queue<TreeNode<int>*> queue;
+    queue.push(root);
+
+    while (!queue.isEmpty()) {
+        TreeNode<int> *front = queue.pop();
+        int n;
+        std::cout << "Enter number of children of " << front -> data << ": ";
+        std::cin >> n;
+        if (n > 0) {
+            std::cout << "Enter " << n << " children of " << front -> data << ": \n";
+        }
+        for (int i = 0; i < n; i++) {
+            std::cin >> data;
+            TreeNode<int> *child = new TreeNode<int>(data);
+            front -> children.push_back(child);
+            queue.push(child);
+        }
     }
     return root;
 }
