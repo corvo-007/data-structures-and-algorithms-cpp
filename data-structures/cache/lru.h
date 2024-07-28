b0fb0129f9f71ffb59d70023f7b09677d8bc3738
deDSeC00720 <terminator.karansharma@gmail.com>
2024-07-28 14:40:00 +0530

    move lru cache to Cache namespace

--- data-structures/cache/lru.h
+++ data-structures/cache/lru.h
@@ -1,110 +1,112 @@
 #include <unordered_map>
 #include "../../common/D_Node.h"
 
-template <typename K, typename V> using CacheNode = D_Node<std::pair<K, V>>;
+namespace Cache {
+	template <typename K, typename V> using CacheNode = D_Node<std::pair<K, V>>;
 
-template <typename K, typename V>
-class LRUCache {
-	CacheNode<K, V> *head;
-	CacheNode<K, V> *tail;
-	int _capacity;
+	template <typename K, typename V>
+	class LRUCache {
+		CacheNode<K, V> *head;
+		CacheNode<K, V> *tail;
+		int _capacity;
 
-	std::unordered_map<K, CacheNode<K, V>*> node_map;
+		std::unordered_map<K, CacheNode<K, V>*> node_map;
 
-public:
-	LRUCache(int _capacity): head(nullptr), tail(nullptr), _capacity(_capacity) {
+	public:
+		LRUCache(int _capacity): head(nullptr), tail(nullptr), _capacity(_capacity) {
 
-	}
+		}
 
-private:
-	void push_front(CacheNode<K, V> *node_ptr) {
-		if (!head) {
+	private:
+		void push_front(CacheNode<K, V> *node_ptr) {
+			if (!head) {
+				head = node_ptr;
+				tail = node_ptr;
+				return ;
+			}
+
+			node_ptr -> next = head;
+			head -> prev = node_ptr;
 			head = node_ptr;
-			tail = node_ptr;
-			return ;
 		}
 
-		node_ptr -> next = head;
-		head -> prev = node_ptr;
-		head = node_ptr;
-	}
+		void makeRecent(CacheNode<K, V> *node_ptr) {
+			if (head == node_ptr) {
+				return ;
+			}
 
-	void makeRecent(CacheNode<K, V> *node_ptr) {
-		if (head == node_ptr) {
-			return ;
+			CacheNode<K, V> *prev = node_ptr -> prev;
+			CacheNode<K, V> *next = node_ptr -> next;
+
+			prev -> next = next;
+			if (next) {
+				next -> prev = prev;
+			}
+			else {
+				tail = prev;
+			}
+
+			node_ptr -> prev = nullptr;
+			node_ptr -> next = head;
+			head -> prev = node_ptr;
+			head = node_ptr;
 		}
 
-		CacheNode<K, V> *prev = node_ptr -> prev;
-		CacheNode<K, V> *next = node_ptr -> next;
+		void pop_back() {
+			if (!head) {
+				return ;
+			}
+			if (head == tail) {
+				delete head;
+				head = nullptr;
+				tail = nullptr;
+				return ;
+			}
 
-		prev -> next = next;
-		if (next) {
-			next -> prev = prev;
-		}
-		else {
-			tail = prev;
-		}
-
-		node_ptr -> prev = nullptr;
-		node_ptr -> next = head;
-		head -> prev = node_ptr;
-		head = node_ptr;
-	}
-
-	void pop_back() {
-		if (!head) {
-			return ;
-		}
-		if (head == tail) {
-			delete head;
-			head = nullptr;
-			tail = nullptr;
-			return ;
+			CacheNode<K, V> *temp = tail;
+			tail = tail -> prev;
+			tail -> next = nullptr;
+			delete temp;
 		}
 
-		CacheNode<K, V> *temp = tail;
-		tail = tail -> prev;
-		tail -> next = nullptr;
-		delete temp;
-	}
+	public:
+		void put(K key, V value) {
+			if (node_map.count(key)) {
+				node_map[key] -> data.second = value;
+				makeRecent(node_map[key]);
+				return ;
+			}
 
-public:
-	void put(K key, V value) {
-		if (node_map.count(key)) {
-			node_map[key] -> data.second = value;
+			if (node_map.size() == _capacity) {
+				node_map.erase(tail -> data.first);
+				pop_back();
+			}
+
+			CacheNode<K, V> *newNode = new CacheNode<K, V>({ key, value });
+
+			node_map[key] = newNode;
+			push_front(newNode);
+		}
+
+		V get(K key) {
+			if (!node_map.count(key)) {
+				return 0;
+			}
+			V value = node_map[key] -> data.second;
 			makeRecent(node_map[key]);
-			return ;
+			return value;
 		}
 
-		if (node_map.size() == _capacity) {
-			node_map.erase(tail -> data.first);
-			pop_back();
+		int size() {
+			return node_map.size();
 		}
 
-		CacheNode<K, V> *newNode = new CacheNode<K, V>({ key, value });
-
-		node_map[key] = newNode;
-		push_front(newNode);
-	}
-
-	V get(K key) {
-		if (!node_map.count(key)) {
-			return 0;
+		int capacity() {
+			return _capacity;
 		}
-		V value = node_map[key] -> data.second;
-		makeRecent(node_map[key]);
-		return value;
-	}
 
-	int size() {
-		return node_map.size();
-	}
-
-	int capacity() {
-		return _capacity;
-	}
-
-	bool empty() {
-		return node_map.empty();
-	}
-};
+		bool empty() {
+			return node_map.empty();
+		}
+	};
+}
