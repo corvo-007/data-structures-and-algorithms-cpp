dd7686e96c104ea4f1d7931ffbd1083595cc6bb9
deDSeC00720 <terminator.karansharma@gmail.com>
2025-01-03 04:00:44 +0530

    add optimised approach for minimum interval to include each query

--- problems/intervals/minimum-interval-to-include-each-query.h
+++ problems/intervals/minimum-interval-to-include-each-query.h
@@ -1,3 +1,6 @@
+#include <algorithm>
+#include <queue>
+#include <unordered_map>
 #include <vector>
 
 namespace Intervals {
@@ -13,6 +16,39 @@ namespace Intervals {
             }
 
             output.push_back(i != INT_MAX ? i : -1);
+        }
+
+        return output;
+    }
+
+    std::vector<int> minInterval(std::vector<std::vector<int>> &intervals, std::vector<int> &queries) {
+        auto sortedQueries = queries;
+
+        std::sort(intervals.begin(), intervals.end());
+        std::sort(sortedQueries.begin(), sortedQueries.end());
+
+        std::priority_queue<std::pair<int, int>, std::vector<std::pair<int, int>>, std::greater<std::pair<int, int>>> minHeap;
+        std::unordered_map<int, int> map;
+
+        int i = 0;
+
+        for (const int &query : sortedQueries) {
+            while (i < intervals.size() && query >= intervals[i][0]) {
+                minHeap.push({intervals[i][1] - intervals[i][0] + 1, intervals[i][1]});
+                i++;
+            }
+
+            while (!minHeap.empty() && minHeap.top().second < query) {
+                minHeap.pop();
+            }
+
+            map[query] = !minHeap.empty() ? minHeap.top().first : -1;
+        }
+
+        std::vector<int> output;
+
+        for (const int &query : queries) {
+            output.push_back(map[query]);
         }
 
         return output;
