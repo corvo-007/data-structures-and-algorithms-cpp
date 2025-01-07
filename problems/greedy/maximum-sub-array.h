9037fa14af5efddb78fadc4edc2f3b5321b5ed84
deDSeC00720 <terminator.karansharma@gmail.com>
2025-01-07 14:27:13 +0530

    add maximum sub array

---
+++ problems/greedy/maximum-sub-array.h
@@ -0,0 +1,18 @@
+#include <vector>
+
+namespace Greedy {
+    int maxSubArray(const std::vector<int> &vec) {
+        int max = INT_MIN;
+        int curr_sum = 0;
+
+        for (const int &n : vec) {
+            curr_sum += n;
+            max = std::max(max, curr_sum);
+            if (curr_sum < 0) {
+                curr_sum = 0;
+            }
+        }
+
+        return max;
+    }
+}
