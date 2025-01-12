1621051beadd3421b982bff5c773fe4fbc42ca59
deDSeC00720 <terminator.karansharma@gmail.com>
2025-01-12 17:27:03 +0530

    add set matrix zeroes (in place)

--- problems/math-and-geometry/set-matrix-zeroes.h
+++ problems/math-and-geometry/set-matrix-zeroes.h
@@ -27,4 +27,41 @@ namespace MathAndGeometry {
             }
         }
     }
+
+    void setZeroes(std::vector<std::vector<int>> &matrix) {
+        int firstrow = false, firstcol = false;
+        for (int i = 0; i < matrix.size(); i++) {
+            for (int j = 0; j < matrix[0].size(); j++) {
+                if (matrix[i][j] == 0) {
+                    if (i == 0) {
+                        firstrow = true;
+                    }
+                    if (j == 0) {
+                        firstcol = true;
+                    }
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+
+        for (int i = 1; i < matrix.size(); i++) {
+            for (int j = 1; j < matrix[0].size(); j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        if (firstrow) {
+            for (int i = 1; i < matrix[0].size(); i++) {
+                matrix[0][i] = 0;
+            }
+        }
+        if (firstcol) {
+            for (int i = 1; i < matrix.size(); i++) {
+                matrix[i][0] = 0;
+            }
+        }
+    }
 }
