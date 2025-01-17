5c489cc4cd507c7d07b4ae0fc66c91fe49857d5a
deDSeC00720 <terminator.karansharma@gmail.com>
2025-01-18 00:15:24 +0530

    add network delay timer dijkstra

--- problems/advanced-graphs/network-delay-time.h
+++ problems/advanced-graphs/network-delay-time.h
@@ -54,4 +54,43 @@ namespace AdvancedGraphs {
 
         return -1;
     }
+
+    int networkDelayTime_dijkstra(const std::vector<std::vector<int>> &times, int n, int k) {
+        std::unordered_map<int, std::vector<std::pair<int, int>>> network;
+
+        for (auto time : times) {
+            network[time[0]].emplace_back(time[1], time[2]);
+        }
+
+        std::unordered_set<int> visited;
+        std::priority_queue<std::pair<int, int>, std::vector<std::pair<int, int>>, std::greater<std::pair<int, int>>> pq;
+
+        pq.push({0, k});
+
+        int time = 0;
+
+        while (!pq.empty()) {
+            auto [ w, n ] = pq.top();
+            pq.pop();
+
+            if (visited.count(n)) {
+                continue;
+            }
+
+            visited.insert(n);
+            time = w;
+
+            if (!network.count(n)) {
+                continue;
+            }
+
+            for (auto edge : network[n]) {
+                if (!visited.count(edge.first)) {
+                    pq.push({ w + edge.second, edge.first });
+                }
+            }
+        }
+
+        return visited.size() == n ? time : -1;
+    }
 }
