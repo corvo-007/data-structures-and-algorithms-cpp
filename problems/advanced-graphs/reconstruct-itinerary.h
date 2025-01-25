8fba506ccf60e2d7e1c508c0a1082aebcc408ea7
deDSeC00720 <terminator.karansharma@gmail.com>
2025-01-25 22:02:42 +0530

    add reconstruct itinerary (hierholzer)

--- problems/advanced-graphs/reconstruct-itinerary.h
+++ problems/advanced-graphs/reconstruct-itinerary.h
@@ -4,6 +4,7 @@
 #include <map>
 #include <unordered_set>
 #include <queue>
+#include <algorithm>
 
 namespace AdvancedGraphs {
     bool dfs(std::unordered_map<std::string, std::map<std::string, int>> &graph, std::string airport, int used_tickets, int total_tickets, std::vector<std::string> &output) {
@@ -44,6 +45,38 @@ namespace AdvancedGraphs {
         std::vector<std::string> output;
 
         dfs(graph, "JFK", 0, total_tickets, output);
+
+        return output;
+    }
+
+    void hierholzer(std::unordered_map<std::string, std::map<std::string, int>> &graph, std::string airport, std::vector<std::string> &output) {
+        if (!graph.count(airport)) {
+            output.push_back(airport);
+            return ;
+        }
+
+        for (auto &edge : graph[airport]) {
+            if (edge.second > 0) {
+                edge.second--;
+                hierholzer(graph, edge.first, output);
+            }
+        }
+
+        output.push_back(airport);
+    }
+
+    std::vector<std::string> findItinerary_optimised(const std::vector<std::vector<std::string>> &tickets) {
+        std::unordered_map<std::string, std::map<std::string, int>> graph;
+
+        for (const auto &ticket : tickets) {
+            graph[ticket[0]][ticket[1]]++;
+        }
+
+        std::vector<std::string> output;
+
+        hierholzer(graph, "JFK",output);
+
+        std::reverse(output.begin(), output.end());
 
         return output;
     }
