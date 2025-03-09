16128b378d0d6ec41040800bdfbb48ab7f028cee
deDSeC00720 <terminator.karansharma@gmail.com>
2025-03-09 15:54:56 +0530

    add cheapest flights within k stops

---
+++ problems/advanced-graphs/cheapest-flights-within-k-stops.h
@@ -0,0 +1,23 @@
+#include <vector>
+
+namespace AdvancedGraphs {
+    int findCheapestPrice(int n, const std::vector<std::vector<int>> &flights, int src, int dest, int k) {
+        std::vector<int> prices(n, INT_MAX);
+
+        prices[src] = 0;
+
+        for (int i = 0; i <= k; i++) {
+            std::vector<int> tempPrices = prices;
+
+            for (const std::vector<int> &flight : flights) {
+                if (prices[flight[0]] != INT_MAX && prices[flight[0]] + flight[2] < tempPrices[flight[1]]) {
+                    tempPrices[flight[1]] = prices[flight[0]] + flight[2];
+                }
+            }
+
+            prices = tempPrices;
+        }
+
+        return prices[dest] != INT_MAX ? prices[dest] : -1;
+    }
+}
